5df5ecd7ec21365efa40e595653007c59f061196
Jan Müller <janmueller3698@gmail.com>
2022-02-13 16:19:40 +0100

    feat: improve markdown linting

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ module.exports = {
   extends: [
     '@antfu',
     'plugin:markdown/recommended',
+    'plugin:md/recommended',
     'plugin:prettier/recommended',
     'plugin:yml/standard',
     'plugin:yml/prettier',
   ],
   overrides: [
+    {
+      files: ['*.md'],
+      parser: 'markdown-eslint-parser',
+      rules: {
+        'prettier/prettier': [
+          'error',
+          // Important to force prettier to use "markdown" parser - otherwise it wouldn't be able to parse *.md files.
+          // You also can configure other options supported by prettier here - "prose-wrap" is
+          // particularly useful for *.md files
+          { parser: 'markdown' },
+        ],
+      },
+    },
     {
       files: ['*.yaml', '*.yml'],
       parser: 'yaml-eslint-parser',
