2ffbf1c8944fc0e728de4bb5ded545a4041e6496
Jan Müller <janmueller3698@gmail.com>
2022-02-13 15:49:24 +0100

    feat: lint markdown

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@antfu'],
+  extends: ['@antfu', 'plugin:markdown/recommended'],
   rules: {
     'brace-style': ['error', '1tbs'],
     '@typescript-eslint/brace-style': ['error', '1tbs'],
