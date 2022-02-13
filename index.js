8e4b6617724c98934da5d3e12cf1d40c01c38599
Jan Müller <janmueller3698@gmail.com>
2022-02-13 16:13:18 +0100

    feat: lint yml

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 module.exports = {
-  extends: ['@antfu', 'plugin:markdown/recommended'],
-  plugins: ['prettier'],
+  extends: [
+    '@antfu',
+    'plugin:markdown/recommended',
+    'plugin:prettier/recommended',
+    'plugin:yml/standard',
+    'plugin:yml/prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.yaml', '*.yml'],
+      parser: 'yaml-eslint-parser',
+    },
+  ],
   rules: {
     '@typescript-eslint/brace-style': ['error', '1tbs'],
     'brace-style': ['error', '1tbs'],
-    'prettier/prettier': 'error',
   },
 }
